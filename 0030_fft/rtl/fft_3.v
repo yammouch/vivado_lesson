2b5d7be488b9fbbe55820d279b2a33d0f25f8a69
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-04-12 16:42:22 +0900

    Updated.

--- 0030_fft/rtl/fft_3.v
+++ 0030_fft/rtl/fft_3.v
@@ -1,10 +1,10 @@
 module fft_3 #(
- parameter DBW = 3,
+ parameter DBW = 4,
  parameter CBW = 3) (
  input              clk,
  input              rstx,
  input              clear,
- input  [2*DBW-1:0] din,
+ input  [  DBW-1:0] din,
  output [2*DBW-1:0] dout );
 
 reg [CBW-1:0] cnt;
@@ -18,21 +18,30 @@ wire [2*DBW-1:0] dout_1, dout_2;
 stage1 stage1_1 #(.DBW(DBW), .CBW(CBW)) (
  .clk    (clk),
  .cnt    (cnt),
- .trigon (...), // to be generated from a sub program
- .din    (din),
+ .trigon ({ 4'b0000, 4'b0100
+          , 4'b0000, 4'b0100
+          , 4'b0000, 4'b0100
+          , 4'b0000, 4'b0100 }),
+ .din    ({{DBW{1'b0}}, din}),
  .dout   (dout_1) );
 
 stage1 stage1_2 #(.DBW(DBW), .CBW(CBW)) (
  .clk    (clk),
  .cnt    ({^cnt[CBW-1], cnt[CBW-2:0]}),
- .trigon (...), // to be generated from a sub program
+ .trigon ({ 4'b0000, 4'b0100
+          , 4'b0100, 4'b0000
+          , 4'b0000, 4'b0100
+          , 4'b0100, 4'b0000 }),
  .din    (dout_1),
  .dout   (dout_2) );
 
 stage1 stage1_3 #(.DBW(DBW), .CBW(CBW)) (
  .clk    (clk),
  .cnt    (cnt),
- .trigon (...), // to be generated from a sub program
+ .trigon ({ 4'b0000, 4'b0100
+          , 4'b0100, 4'b0000
+          , 4'b0011, 4'b0011
+          , 4'b0011, 4'b1011 }),
  .din    (dout_2),
  .dout   (dout) );
 
