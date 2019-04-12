605ac1461bc29128660f55678294969ef85d206e
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-04-12 17:36:03 +0900

    First checkin.

---
+++ 0030_fft/sim/t900.v
@@ -0,0 +1,25 @@
+`timescale 1ps/1ps
+
+module tb;
+
+`include "inst_fft_3.v"
+`include "tasks.v"
+
+task test_main;
+begin
+  init; #50_000;
+  cg.en = 1'b1;
+  repeat (4) @(negedge clk);
+  rstx = 1'b1;
+  repeat (4) @(negedge clk);
+  clear = 1'b1;
+  repeat (64) @(negedge clk);
+  cg.en = 1'b0;
+end
+endtask
+
+initial begin
+  test_main;
+end
+
+endmodule
