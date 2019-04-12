8dc11e6927a74b5d70ca008b77c4058bcbdf93c1
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-04-12 17:17:29 +0900

    Updated.

--- 0030_fft/rtl/stage1.v
+++ 0030_fft/rtl/stage1.v
@@ -1,11 +1,36 @@
 module stage1 #(
- parameter DBW = 3,
+ parameter DBW = 4,
  parameter CBW = 3 ) (
  input                       clk,
  input  [           CBW-1:0] cnt,
  input  [2*DBW*(1<<CBW)-1:0] trigon,
- input  [         2*DBW-1:0] din,
- output [         2*DBW-1:0] dout );
+ input  [         2*DBW-1:0] din,    // s1.DBW-2
+ output [         2*DBW-1:0] dout ); // s1.DBW-2
+
+function [DBW*2:0] addsub(
+ input [  DBW-1:0] din1,
+ input [DBW*2-1:0] din2,
+ input             sub);
+// s1.DBW-2 + s3.DBW*2-4 -> s4.DBW*2-4
+  addsub
+   = {din1[DBW-1], din1[DWB-1:0], {(DBW){1'b0}}}
+   + ({(2*DBW+1){sub}} ^ {din2[2*DBW-1], din2[2*DBW-1:0]})
+   + {{(2*DBW){1'b0}}, sub};
+endfuncion
+
+function [DBW+2:0] round(input [DBW*2:0] din)
+  round = {din[2*DWB], din[2*DWB:DWB-1]} + {{(DBW+2){1'b0}}, din[DWB-2]};
+endfunction
+
+function [DBW-1:0] clip(input [DBW+2:0] din);
+// s5.DBW-3 to s2.DBW-3
+  if      (!din[DBW+2] && din[DBW+1:DBW] != 2'b00)
+    clip = {1'b0, {(DBW-1){1'b1}}};
+  else if ( din[DBW+2] && din[DBW+1:DBW] != 2'b11)
+    clip = {1'b1, {(DBW-1){1'b0}}};
+  else
+    clip = din[DBW-1:0];
+endfunction
 
 reg [2*DBW-1:0] mem [(1 << (CBW-1))-1:0];
 reg [2*DBW-1:0] mem_rd;
@@ -24,32 +49,19 @@ halfrate halfrate_i #( .DBW(2*DBW), .CBW(CBW) ) (
  .din  (din),
  .dout (dout_half) );
 
-wire [4*DBW-1:0] prod;
+wire [4*DBW-1:0] prod; // s3.DBW*2-4
 cmplxmul cmplxmul_i #( .DBW(DBW) ) (
  .op1  (trigon >> ({~cnt[CBW-1], cnt[CBW-2:1]}*2*DBW)),
  .op2  (dout_half),
  .prod (prod) );
 
-wire [2*DBW:0] sum_im
- = {din[2*DBW-1], din[2*DWB-1:DWB], {(DBW){1'b0}}}
- + ({(2*DBW+1){cnt[0]}} ^ {prod[4*DBW-1], prod[4*DBW-1:2*DBW]})
- + {{(2*DBW){1'b0}}, cnt[0]};
-wire [DBW+1:0] sum_im_round
- = {sum_im[2*DWB], sum_im[2*DWB:DWB]} + {{(DBW+1){1'b0}}, sum_im[DWB-1]};
-assign dout[2*DWB-1:DWB]
- = sum_im_round[DBW+1:DBW] == 2'b01 ? {1'b0, {(DBW-1){1'b1}}}
- : sum_im_round[DBW+1:DBW] == 2'b10 ? {1'b1, {(DBW-1){1'b0}}}
- :                                    sum_im_round[DBW-1:0];
+wire [2*DBW:0] sum_im = addsum(din[2*DBW-1:DBW], prod[4*DBW-1:2*DBW], cnt[0]);
+// ^-- s4.DBW*2-4
+wire [DBW+2:0] sum_im_round = round(sum_im) // s5.DBW-3
+assign dout[2*DWB-1:DWB] = clip(sum_im_round); // s2.DBW-3
 
-wire [2*DWB:0] sum_re
- = {din[DBW-1], din[DWB-1:0], {(DBW){1'b0}}}
- + ({(2*DBW+1){~cnt[0]}} ^ {prod[2*DBW-1], prod[2*DBW-1:0]})
- + {{(2*DBW){1'b0}}, ~cnt[0]};
-wire [DBW+1:0] sum_re_round
- = {sum_re[2*DWB], sum_re[2*DWB:DWB]} + {{(DBW+1){1'b0}}, sum_re[DWB-1]};
-assign dout[DWB-1:0]
- = sum_re_round[DBW+1:DBW] == 2'b01 ? {1'b0, {(DBW-1){1'b1}}}
- : sum_re_round[DBW+1:DBW] == 2'b10 ? {1'b1, {(DBW-1){1'b0}}}
- :                                    sum_re_round[DBW-1:0];
+wire [2*DWB:0] sum_re = addsum(din[DBW-1:0], prod[2*DBW-1:0], ~cnt[0]);
+wire [DBW+1:0] sum_re_round = round(sum_re);
+assign dout[DWB-1:0] = clip(sum_re_round);
 
 endmodule
