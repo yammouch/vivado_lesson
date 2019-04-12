1afb1363ea6fdd21a2e7f8a2066c541123e5e4ea
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-04-12 16:09:53 +0900

    Updated.

--- 0030_fft/rtl/stage1.v
+++ 0030_fft/rtl/stage1.v
@@ -1,15 +1,11 @@
 module stage1 #(
  parameter DBW = 3,
  parameter CBW = 3 ) (
- input              clk,
- input              clear,
- input  [2*DBW-1:0] din,
- output [2*DBW-1:0] dout );
-
-reg [CBW-1:0] cnt;
-always @(posedge clk)
-  if (clear) cnt <= 0;
-  else       cnt <= cnt + 1;
+ input                       clk,
+ input  [           CBW-1:0] cnt,
+ input  [2*DBW*(1<<CBW)-1:0] trigon,
+ input  [         2*DBW-1:0] din,
+ output [         2*DBW-1:0] dout );
 
 reg [2*DBW-1:0] mem [(1 << (CBW-1))-1:0];
 reg [2*DBW-1:0] mem_rd;
@@ -30,8 +26,30 @@ halfrate halfrate_i #( .DBW(2*DBW), .CBW(CBW) ) (
 
 wire [4*DBW-1:0] prod;
 cmplxmul cmplxmul_i #( .DBW(DBW) ) (
- .op1  (table_rd), // table_rd is needed be defined.
+ .op1  (trigon >> ({~cnt[CBW-1], cnt[CBW-2:1]}*2*DBW)),
  .op2  (dout_half),
  .prod (prod) );
+
+wire [2*DBW:0] sum_im
+ = {din[2*DBW-1], din[2*DWB-1:DWB], {(DBW){1'b0}}}
+ + ({(2*DBW+1){cnt[0]}} ^ {prod[4*DBW-1], prod[4*DBW-1:2*DBW]})
+ + {{(2*DBW){1'b0}}, cnt[0]};
+wire [DBW+1:0] sum_im_round
+ = {sum_im[2*DWB], sum_im[2*DWB:DWB]} + {{(DBW+1){1'b0}}, sum_im[DWB-1]};
+assign dout[2*DWB-1:DWB]
+ = sum_im_round[DBW+1:DBW] == 2'b01 ? {1'b0, {(DBW-1){1'b1}}}
+ : sum_im_round[DBW+1:DBW] == 2'b10 ? {1'b1, {(DBW-1){1'b0}}}
+ :                                    sum_im_round[DBW-1:0];
+
+wire [2*DWB:0] sum_re
+ = {din[DBW-1], din[DWB-1:0], {(DBW){1'b0}}}
+ + ({(2*DBW+1){~cnt[0]}} ^ {prod[2*DBW-1], prod[2*DBW-1:0]})
+ + {{(2*DBW){1'b0}}, ~cnt[0]};
+wire [DBW+1:0] sum_re_round
+ = {sum_re[2*DWB], sum_re[2*DWB:DWB]} + {{(DBW+1){1'b0}}, sum_re[DWB-1]};
+assign dout[DWB-1:0]
+ = sum_re_round[DBW+1:DBW] == 2'b01 ? {1'b0, {(DBW-1){1'b1}}}
+ : sum_re_round[DBW+1:DBW] == 2'b10 ? {1'b1, {(DBW-1){1'b0}}}
+ :                                    sum_re_round[DBW-1:0];
 
 endmodule
