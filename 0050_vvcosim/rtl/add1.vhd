cfad55b5015e2bb3ecddc0a4b1358e4832c2dde9
Takuya Yamauchi <tyama@preferred.jp>
2019-08-26 12:10:58 +0900

    File name has been changed

---
+++ 0050_vvcosim/rtl/add1.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric.all;
+
+entity dut is
+  port (
+   clk  : in  std_logic;
+   rstx : in  std_logic;
+   din  : in  std_logic_vector(3 downto 0);
+   dout : out std_logic_vector(3 downto 0)
+  );
+end entity;
+
+architecture rtl of dut is
+  signal dout_r : std_logic_vector(3 downto 0);
+begin
+  process (clk, reset) begin
+    if rstx = '0' then
+      dout_r <= (others => '0');
+    else
+      dout_r <= std_logic_vector(unsigned(din) + 1);
+    end if;
+  end process;
+end rtl;
